6b5bd825b6d6fcd1dda4b033a4c2e7dd8cd7cb78
Leonhard Bergold <leonhard.bergold@outlook.com>
2025-05-28 19:38:59 +0200

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,13 +1,15 @@
 const TEST_CALCULATIONS = ["12.5 + 7.5", "70 - 30", "10 * 3", "1.5 / 0.5", "2^4", "5*-(2+2)"];
 
-let position = 0;
-
 export function slide(slider, direction) {
   const slides = [...slider.children];
+
+  let position = parseInt(slider.dataset.position) || 0;
 
   if (direction === "right" && position === -(slides.length - 1)) position = 0;
   else if (direction === "left" && position === 0) position = -(slides.length - 1);
   else direction === "right" ? position-- : position++;
+
+  slider.dataset.position = position;
 
   slides.forEach(slide => {
     slide.style.transform = `translateX(${100 * position}%)`;
@@ -19,6 +21,8 @@ export function openOrCloseModal(modal, modalOverlay) {
   modalOverlay.classList.toggle("active");
 }
 
-export function getTestCalculation() {
+export function getTestCalculation(slider) {
+  console.log(slider);
+  const position = parseInt(slider.dataset.position) || 0;
   return TEST_CALCULATIONS[-position];
 }
